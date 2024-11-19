b9995e2eead3fc4a7e33693317e954534288ba61
Enzo Sanchez <enzo.sanchez@epfedu.fr>
2024-11-19 23:35:11 +0100

    TP 3

---
+++ TP3_Heroic_Fantasy_SANCHEZ/src/arme/arme.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arme;
+
+/**
+ *
+ * @author enzos
+ */
+
+public abstract class arme {
+    private String nom;
+    private int niveauAttaque;
+
+    public arme(String nom, int niveauAttaque) {
+        this.nom = nom;
+        this.niveauAttaque = Math.min(niveauAttaque, 100);
+    }
+    public int getNiveauAttaque() {
+        return niveauAttaque;
+    }
+    public String getNom() {
+        return nom;
+    }
+    @Override
+    public String toString() {
+        return "Arme: " + nom + ", Niveau d'attaque: " + niveauAttaque;
+    }
+}
